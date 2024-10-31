092c45328eb4ee2d75a2da9acc6d7dd49fd55abb
Paula Castillo <paula.castilloo@usm.cl>
2024-10-31 16:14:36 -0300

    ups

--- src/src/components/layout.jsx
+++ src/src/components/layout.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePage from '../pages/home_page'
 import LightbulbPage from '../pages/lightbulb_page'
 import NavBar from '../components/nav_bar'
-import WelcomeScreen from '../components/welcome_screen'
+import WelcomeScreen from '../pages/welcome_screen'
 
 const Layout = () => {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-gray-100">
+      <div className="min-h-screen items-center bg-gray-100">
         <NavBar />
-        <div className="container mx-auto px-4 py-8">
+        <div className="container items-center mx-auto px-4 py-8">
           <Routes>
             <Route path="/" element={<WelcomeScreen />} />
-            <Route path="/home" element={<HomePage />} />
+
             <Route path="/lightbulb" element={<LightbulbPage />} />
           </Routes>
         </div>
