c510f7b3abf4cba4afa02562586bd977bebfd4df
Paula Castillo <paula.castilloo@usm.cl>
2024-11-02 20:53:19 -0300

    cambios
    Intento de cambios para centrar nuevamente el inicio

--- src/pages/welcome_screen.jsx
+++ src/pages/welcome_screen.jsx
@@ -13,7 +13,7 @@ const WelcomeScreen = () => {
   }
 
   return (
-    <div className="welcome">
+    <div className='welcome_screen'>
       <h1 className="text-4xl items-center font-bold mb-8">¡Bienvenido a ColaBora!</h1>
       <p className="text-xl mb-8">¿De qué tipo de organización eres miembro?</p>
       <div className="flex mb-8">
@@ -31,7 +31,7 @@ const WelcomeScreen = () => {
         </button>
       </div>
       <button
-        onClick={() => navigate('/home_page')}//Este ya no sirve es el que estaba de ejemplo
+        onClick={() => navigate('/home')}//Este ya no sirve es el que estaba de ejemplo
         className="px-6 py-2 bg-gray-800 text-white rounded-full hover:bg-gray-700"
       >
         Ingresar
